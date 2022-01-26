84287193cd3ea32b4a2d03acae9ad8849d2a49d4
LeoThayer <92405073+LeoThayer@users.noreply.github.com>
2022-01-26 16:44:12 -0600

    New Buttons

--- New Robot Timed/src/main/java/frc/robot/subsystems/ShooterPathMovement.java
+++ New Robot Timed/src/main/java/frc/robot/subsystems/ShooterPathMovement.java
@@ -3,26 +3,25 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-public class ShooterPathMovement {
+public class ShooterPathMovement extends SubsystemBase {
     private CANSparkMax flyWheelMotor = new CANSparkMax(Constants.FLY_WHEEL_MOTOR_DEVICE_ID, MotorType.kBrushless);
-    private CANSparkMax internalMoverMotor = new CANSparkMax(Constants.BALL_MOVER_MOTOR_DEVICE_ID, MotorType.kBrushless);
+    private CANSparkMax internalMoverMotor = new CANSparkMax(Constants.BALL_MOVER_MOTOR_DEVICE_ID,
+            MotorType.kBrushless);
     private CANSparkMax intakeMotor = new CANSparkMax(Constants.INTAKE_MOTOR_DEVICE_ID, MotorType.kBrushless);
 
-    /**top of shooter wheel speeds (go pew pew fast)*/
-    public void setFlySpeed(double speed)
-    {
+    /** top of shooter wheel speeds (go pew pew fast) */
+    public void setFlySpeed(double speed) {
         flyWheelMotor.set(speed);
     }
 
-    public void setMoverSpeed(double speed)
-    {
+    public void setMoverSpeed(double speed) {
         internalMoverMotor.set(speed);
     }
 
-    public void setIntakeSpeed(double speed)
-    {
+    public void setIntakeSpeed(double speed) {
         intakeMotor.set(speed);
     }
 

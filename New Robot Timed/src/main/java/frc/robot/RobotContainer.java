96d87a14b3b0e88505743a4368f48214dfc541fc
EmilyKram <90582499+EmilyKram@users.noreply.github.com>
2022-02-16 19:00:25 -0600

    Jake Code

--- New Robot Timed/src/main/java/frc/robot/RobotContainer.java
+++ New Robot Timed/src/main/java/frc/robot/RobotContainer.java
@@ -1,109 +1,55 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.command.DoNotMove;
-import frc.robot.command.SetFlySpeed;
-import frc.robot.command.SetFlySpeedUsingCalculation;
-import frc.robot.command.SetIntakeSpeed;
-import frc.robot.command.SetInternalMoveSpeed;
-import frc.robot.command.SpitBalls;
-import frc.robot.command.Targeting;
-import frc.robot.command.TaxiAndShoot;
-import frc.robot.command.TaxiTarmac;
-import frc.robot.command.hookcommands.AngledHookJoystick;
-import frc.robot.command.hookcommands.MoveCloserToNinetyDegrees;
-import frc.robot.command.hookcommands.MoveCloserToZeroDegrees;
-import frc.robot.command.hookcommands.PullVertHooksIn;
-import frc.robot.command.hookcommands.ReachVertHooksUp;
+import frc.robot.commands.Drive;
+import frc.robot.commands.ResetDriveTrainPosition;
 import frc.robot.subsystems.DriveTrain;
-import frc.robot.subsystems.Hooks;
-import frc.robot.subsystems.Limelight;
-import frc.robot.subsystems.ShooterPathMovement;
 
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
 public class RobotContainer {
   Joystick leftStick = new Joystick(Constants.LEFT_STICK_PORT);
   Joystick midStick = new Joystick(Constants.MID_STICK_PORT);
   Joystick rightStick = new Joystick(Constants.RIGHT_STICK_PORT);
 
-  // subsystems
   DriveTrain vroomVroom = new DriveTrain();
-  ShooterPathMovement pewPew = new ShooterPathMovement();
-  Hooks climber = new Hooks();
-  Limelight vision = new Limelight();
+  Drive drive = new Drive(rightStick, midStick, vroomVroom);
+  ResetDriveTrainPosition resetDriveTrainPosition = new ResetDriveTrainPosition(vroomVroom);
+  Command move = resetDriveTrainPosition.andThen(drive);
 
-  // commands
-  DoNotMove doNotMove = new DoNotMove(vroomVroom, pewPew);
-  TaxiAndShoot taxiAndShoot = new TaxiAndShoot(vroomVroom, pewPew);
-  TaxiTarmac taxiTarmac = new TaxiTarmac(vroomVroom);
-  AngledHookJoystick angledHookJoystick = new AngledHookJoystick(climber, rightStick);
-  ReachVertHooksUp reachVertHooksUp = new ReachVertHooksUp(climber);
-  PullVertHooksIn pullVertHooksIn = new PullVertHooksIn(climber);
-  MoveCloserToNinetyDegrees moveCloserToNinetyDegrees = new MoveCloserToNinetyDegrees(climber);
-  MoveCloserToZeroDegrees moveCloserToZeroDegrees = new MoveCloserToZeroDegrees(climber);
-  Targeting targeting = new Targeting(vroomVroom, vision);
-  SetFlySpeed setFlySpeed = new SetFlySpeed(pewPew);
-  SetIntakeSpeed setIntakeSpeed = new SetIntakeSpeed(pewPew);
-  SetInternalMoveSpeed setInternalMoveSpeed = new SetInternalMoveSpeed(pewPew);
-  SpitBalls spitBalls = new SpitBalls(pewPew);
-  SetFlySpeedUsingCalculation setFlySpeedUsingCalculation = new SetFlySpeedUsingCalculation(vision, pewPew);
-
-  ParallelCommandGroup  parallelGroupShootPrep = new ParallelCommandGroup(targeting,setFlySpeed);
-  SequentialCommandGroup shootingGroup = new SequentialCommandGroup(parallelGroupShootPrep, setInternalMoveSpeed);
-
-  // buttons
-  JoystickButton overrideButton = new JoystickButton(rightStick, Constants.HUMAN_OVERRIDE_BUTTON);
-  JoystickButton verticalHookUpBTN = new JoystickButton(rightStick, Constants.VERT_HOOK_UP_BUTTON);
-  JoystickButton verticalHookDownBTN = new JoystickButton(rightStick, Constants.VERT_HOOK_DOWN_BUTTON);
-  JoystickButton angledHookUpBTN = new JoystickButton(rightStick, Constants.ANGLE_HOOK_UP_BUTTON);
-  JoystickButton angledHookDownBTN = new JoystickButton(rightStick, Constants.ANGLE_HOOK_UP_BUTTON);
-  JoystickButton manualIntakeForwardsBTN = new JoystickButton(rightStick, Constants.INTAKEIN_BUTTON);
-  JoystickButton flywheelSpinUpBTN = new JoystickButton(rightStick, Constants.FLYWHEEL_BUTTON);
-  JoystickButton internalFeederInBTN = new JoystickButton(rightStick, Constants.INTERNAL_MOVER_FORWARDS_BUTTON);
-  JoystickButton spitBTN = new JoystickButton(rightStick, Constants.YEET_THE_BALLS_OUT_THE_BACK_BUTTON);
-  JoystickButton fullFIREEEEBTN = new JoystickButton(rightStick, Constants.RIGHT_STICK_TRIGGER);
-
-  // other
-  private final SendableChooser<Command> m_chooser = new SendableChooser<>();
-
-  public RobotContainer() {
+  // The robot's subsystems and commands are defined here...
+ 
+  /** The container for the robot. Contains subsystems, OI devices, and commands. 
+   * @return */
+  public void initialize() {
+    // Configure the button bindings
     configureButtonBindings();
-    begin();
-    vroomVroom.setDefaultCommand(new RunCommand( () -> 
-    vroomVroom.greenLight(midStick.getRawAxis(1), leftStick.getRawAxis(0)), vroomVroom));
+    vroomVroom.setDefaultCommand(drive);
   }
 
-  private void configureButtonBindings() {
-    overrideButton.whileHeld(angledHookJoystick);
-    overrideButton.and(verticalHookUpBTN).whileActiveContinuous(reachVertHooksUp);
-    overrideButton.and(verticalHookDownBTN).whileActiveContinuous(pullVertHooksIn);
-    overrideButton.and(angledHookUpBTN).whileActiveContinuous(moveCloserToNinetyDegrees);
-    overrideButton.and(angledHookDownBTN).whileActiveContinuous(moveCloserToZeroDegrees);
-    overrideButton.and(flywheelSpinUpBTN).whileActiveContinuous(setFlySpeed);
-    overrideButton.and(manualIntakeForwardsBTN).whileActiveContinuous(setIntakeSpeed);
-    overrideButton.and(internalFeederInBTN).whileActiveContinuous(setInternalMoveSpeed);
-    overrideButton.and(spitBTN).whileActiveContinuous(spitBalls);
-    fullFIREEEEBTN.whenHeld(shootingGroup);
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {}
 
-  
-  
-  }
-
-  private void begin() {
-    m_chooser.setDefaultOption("Default Auto", doNotMove);
-    m_chooser.addOption("Taxi + Shoot One", taxiAndShoot);
-    m_chooser.addOption("Taxi", taxiTarmac);
-    SmartDashboard.putData("Auto choices", m_chooser);
-  }
-
-  public Command getAutonomousCommand() {
-    return m_chooser.getSelected();
-  }
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
 
 }
--- New Robot Timed/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ New Robot Timed/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,66 +1,48 @@
 package frc.robot.subsystems;
 
+
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class DriveTrain extends SubsystemBase{
-    private CANSparkMax frontR = new CANSparkMax(Constants.FRONT_RIGHT_MOTOR_DEVICE_ID, MotorType.kBrushless);
-    private CANSparkMax frontL = new CANSparkMax(Constants.FRONT_LEFT_MOTOR_DEVICE_ID, MotorType.kBrushless);
-    private CANSparkMax backR = new CANSparkMax(Constants.BACK_RIGHT_MOTOR_DEVICE_ID, MotorType.kBrushless);
-    private CANSparkMax backL = new CANSparkMax(Constants.BACK_LEFT_MOTOR_DEVICE_ID, MotorType.kBrushless);
+    private CANSparkMax frontR = new CANSparkMax(Constants.FRONT_RIGHT_MOTOR_PORT, MotorType.kBrushless);
+    private CANSparkMax frontL = new CANSparkMax(Constants.BACK_RIGHT_MOTOR_PORT, MotorType.kBrushless);
+    private CANSparkMax backR = new CANSparkMax(Constants.FRONT_LEFT_MOTOR_PORT, MotorType.kBrushless);
+    private CANSparkMax backL = new CANSparkMax(Constants.BACK_LEFT_MOTOR_PORT, MotorType.kBrushless);
     private RelativeEncoder frontREnc = frontR.getEncoder();
     private RelativeEncoder frontLEnc = frontL.getEncoder();
     private RelativeEncoder backREnc = backR.getEncoder();
     private RelativeEncoder backLEnc = backL.getEncoder();
 
-    private MotorControllerGroup leftSideDrive = new MotorControllerGroup(frontR, backR);
-    private MotorControllerGroup rightSideDrive = new MotorControllerGroup(frontL, backL);
-  
-    private DifferentialDrive allDrive = new DifferentialDrive(leftSideDrive, rightSideDrive);
+    MotorControllerGroup leftDrive = new MotorControllerGroup(frontL, backL);
+    MotorControllerGroup rightDrive = new MotorControllerGroup(frontR, backR);
+    MotorControllerGroup motorControllerGroup = new MotorControllerGroup(frontR, frontL, backR, backL);
+    private DifferentialDrive allDrive = new DifferentialDrive(leftDrive, rightDrive);
 
-    private double conversionFactor = 12.75;
 
     public DriveTrain(){
-        frontLEnc.setPositionConversionFactor(conversionFactor);
-        frontREnc.setPositionConversionFactor(conversionFactor);
-        backLEnc.setPositionConversionFactor(conversionFactor);
-        backREnc.setPositionConversionFactor(conversionFactor);
     }
-
-    /**Sets encoder positions to 0 */
+            
     public void resetPosition(){
-        frontLEnc.setPosition(0);
-        frontREnc.setPosition(0);
-        backLEnc.setPosition(0);
-        backREnc.setPosition(0);
+            frontLEnc.setPosition(0d);
+            frontREnc.setPosition(0d);
+            backLEnc.setPosition(0d);
+            backREnc.setPosition(0d);
     }
-
-    public double findPosition(){
-        double encCurrentPosition = frontLEnc.getPosition();
-        encCurrentPosition += frontREnc.getPosition();
-        encCurrentPosition += backREnc.getPosition();
-        encCurrentPosition += backLEnc.getPosition();
-        return encCurrentPosition/4d ;
+    public double getPosition(){
+        double encCurrentPosition = (frontREnc.getPosition());
+        encCurrentPosition =+ (frontLEnc.getPosition());
+        encCurrentPosition =+ (backREnc.getPosition());
+        encCurrentPosition =+ (backLEnc.getPosition());
+        return encCurrentPosition/4d;
     }
-
-
-    /**
-     * Assigns two speeds to the xAxis of the Robot and the Rotation of the Robot. Negative values of xAxis
-     * will move backwards, negative values of the zRotation will rotate counter-clockwise.
-     * Joysticks allow these to be control by controller axes
-     * @param xAxis
-     * @param zRotate
-     */
-    public void greenLight(double xAxis, double zRotate){
-        
-        allDrive.arcadeDrive(Constants.DRIVE_SPEED_MULTIPLIER*xAxis, Constants.DRIVE_SPEED_MULTIPLIER*zRotate);
+    public void greenLight(double zRotate, double xAxis){
+        allDrive.arcadeDrive(Constants.DRIVE_SPEED_MULTIPLIER * zRotate, Constants.DRIVE_SPEED_MULTIPLIER *xAxis);
     }
-
-  
 }

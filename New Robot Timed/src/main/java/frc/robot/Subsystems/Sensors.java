5bff163bae3aa5e3c974e1406daec3c81dd1c02d
EmilyKram <90582499+EmilyKram@users.noreply.github.com>
2022-03-26 09:23:26 -0500

    Create Sensors.java

---
+++ New Robot Timed/src/main/java/frc/robot/Subsystems/Sensors.java
@@ -0,0 +1,11 @@
+package frc.robot.Subsystems;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Sensors extends SubsystemBase{
+    DigitalInput ballInSensor = new DigitalInput(Constants.BALL_IN_SENSOR_PORT);
+    DigitalInput ballOutSensor = new DigitalInput(Constants.BALL_OUT_SENSOR_PORT);
+    
+}

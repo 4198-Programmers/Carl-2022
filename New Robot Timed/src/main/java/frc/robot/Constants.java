f4ef2e083c0f7e127a81974ff5794b5a85d615fe
EmilyKram <90582499+EmilyKram@users.noreply.github.com>
2022-03-25 20:01:54 -0500

    Update Constants.java

--- New Robot Timed/src/main/java/frc/robot/Constants.java
+++ New Robot Timed/src/main/java/frc/robot/Constants.java
@@ -28,10 +28,12 @@ public class Constants {
         public static final double LIMELIGHT_ANGLE_FROM_GROUND = 65;
     /**The limit that the vertical hooks can't go past */
         public static final double VERTICAL_HOOK_TOP_LIMIT = -0;
+        public static final double VERTICAL_HOOK_LOWER_LIMIT = 0;
     /** Used to find the distance as well as the speed */
         public static byte SPEED_MULTIPLIER = 1;
     /**Used to stop the angled hooks from going to opposite direction. */
-        public static final double ANGLED_HOOK_UPPER_LIMIT = 0;
+        public static final double ANGLED_HOOK_UPPER_LIMIT = -97;
+        public static final double ANGLED_HOOK_LOWER_LIMIT = 0;
     /**How far we want the bot to move in Auto */
         public static final double WANTED_AUTO_DISTANCE = 0;
     /**Need the middle wheel Circumference to calculate Spin180 */
@@ -39,8 +41,8 @@ public class Constants {
     //Constant Speeds
         public static final double FEEDER_INTAKE_SPEED = -1;
         public static final double TUNNEL_SPEED = 0.75;
-        public static final double VERTICAL_HOOK_LOWER_LIMIT = 0;
-        public static final double ANGLED_HOOK_LOWER_LIMIT = 0;
+
+
 
 //Targeting Offsets
     public static final double X_OFFSET_TARGETING_TO_THE_RIGHT = 3.75;

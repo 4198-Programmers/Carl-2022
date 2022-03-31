8da8a632bc17b4613ef7f53ded716cd04959b32f
whiteA <whiteA@robocats.com>
2022-03-31 18:47:48 -0500

    Update Maths.java

--- New Robot Timed/src/main/java/frc/robot/Maths.java
+++ New Robot Timed/src/main/java/frc/robot/Maths.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public final class Maths {
 
     public static double rotationConversion(double distanceInInches) {
@@ -47,24 +49,26 @@ public final class Maths {
 
     public static double dakotaVelocity(double YAngle){
         double inchesToMeters = 0.0254;
-        double shooterLaunchAngle = -0;
+        double shooterLaunchAngle = 77;
         double gravitationalVelocity = 9.81;
         double targetHeightInMeters = inchesToMeters * Constants.TARGET_HEIGHT_TOP_TO_GROUND;
         double shooterHeightToGroundMeters = 0.5969;
-        double maxFlywheelVelocity = 10;
+        double maxFlywheelVelocity = 9.6;
         double D = Constants.TARGET_HEIGHT_TOP_TO_GROUND;
         double h = Constants.LIMELIGHT_HEIGHT_TO_GROUND;
         double LLangle = 28.25;
         double totalAngle = Math.tan(Math.toRadians(LLangle + YAngle));
         double distance = (D - h) / (totalAngle);
         double targetDistance = (distance + 24) * inchesToMeters;
+        SmartDashboard.putNumber("Target Distance", targetDistance);
         double radiansForLaunch = Math.toRadians(shooterLaunchAngle);
         double targetVelocityForFlywheel = Math.sqrt((-0.5 * (gravitationalVelocity) 
         * ((Math.pow(targetDistance / Math.cos(radiansForLaunch), 2)))) 
         / (targetHeightInMeters - shooterHeightToGroundMeters 
         - (targetDistance * Math.tan(radiansForLaunch))));
-
+        SmartDashboard.putNumber("Target Velocity", targetVelocityForFlywheel);
         double flywheelTargetSpeed = targetVelocityForFlywheel / maxFlywheelVelocity;
+        SmartDashboard.putNumber("Flywheel Speed", flywheelTargetSpeed);
         return flywheelTargetSpeed;
     }
 

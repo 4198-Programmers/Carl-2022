f07a01ff4b3eba54102f7d38425285e9219be121
EmilyKram <90582499+EmilyKram@users.noreply.github.com>
2022-04-05 08:17:05 -0500

    Update Tunnel.java

--- New Robot Timed/src/main/java/frc/robot/Subsystems/Tunnel.java
+++ New Robot Timed/src/main/java/frc/robot/Subsystems/Tunnel.java
@@ -6,7 +6,7 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-public class TunnelSub extends SubsystemBase {
+public class Tunnel extends SubsystemBase {
     private CANSparkMax tunnelMotor = new CANSparkMax(Constants.TUNNEL_MOTOR_ID, MotorType.kBrushless);
 
     public void setSpeed(double speed) {

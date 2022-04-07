ece9bdbda28c9314cbe8468d10106baf724879a2
EmilyKram <90582499+EmilyKram@users.noreply.github.com>
2022-04-07 10:38:57 -0500

    Update Limelight.java

--- New Robot Timed/src/main/java/frc/robot/subsystems/Limelight.java
+++ New Robot Timed/src/main/java/frc/robot/subsystems/Limelight.java
@@ -1,7 +1,38 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Limelight extends SubsystemBase{
+
+    public enum ChooseLimelightMode{
+        forceOff(0),
+        forceOn(1),
+        undefined(-1);
+        double mode;
+
+    public ChooseLimelightMode(){
+        
+    }
+    }
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    NetworkTableEntry tx = table.getEntry("tx"); // TODO math to convert to inches SOON
+    NetworkTableEntry ty = table.getEntry("ty");
+    NetworkTableEntry tv = table.getEntry("tv");
+    NetworkTableEntry ta = table.getEntry("ta");
+    NetworkTableEntry ts = table.getEntry("ts");
+    NetworkTableEntry tvert = table.getEntry("tvert");
+    NetworkTableEntry tlong = table.getEntry("tlong");
+    NetworkTableEntry tshort = table.getEntry("tshort");
+    NetworkTableEntry ledMode = table.getEntry("ledMode");
+    NetworkTableEntry camMode = table.getEntry("camMode");
+    NetworkTableEntry stream = table.getEntry("stream");
+    NetworkTableEntry Pipeline = table.getEntry("pipeline");
     
+    public double xOffset(){
+       return tx.getDouble(0);
+    }
+
 }

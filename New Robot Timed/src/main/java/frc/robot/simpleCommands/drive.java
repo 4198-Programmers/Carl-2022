9530ad729521f8b7cf88ca8cfbd12338cd0cddef
EmilyKram <90582499+EmilyKram@users.noreply.github.com>
2022-09-14 20:12:41 -0500

    Update drive.java

--- New Robot Timed/src/main/java/frc/robot/simpleCommands/drive.java
+++ New Robot Timed/src/main/java/frc/robot/simpleCommands/drive.java
@@ -5,9 +5,16 @@ import frc.robot.subsystems.driveTrain;
 
 public class drive extends CommandBase{
     public interface drivespeedRetriever{
-
+        double getSpeed();
     }
     driveTrain driveTrain;
     drivespeedRetriever forwardDriveSpeedRetriever;
     drivespeedRetriever sideDriveSpeedRetriever;
+
+    public void Drive(){
+        driveTrain.drive(forwardDriveSpeedRetriever.getSpeed(), sideDriveSpeedRetriever.getSpeed());
+        this.forwardDriveSpeedRetriever = forwardDriveSpeedRetriever;
+        this.sideDriveSpeedRetriever = sideDriveSpeedRetriever;
+        addRequirements(driveTrain);
+    }
 }

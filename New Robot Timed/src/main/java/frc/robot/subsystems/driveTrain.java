ce613bd11822cc981e62edd7624e95cea775a213
EmilyKram <90582499+EmilyKram@users.noreply.github.com>
2022-09-14 20:00:28 -0500

    Create driveTrain.java

---
+++ New Robot Timed/src/main/java/frc/robot/subsystems/driveTrain.java
@@ -0,0 +1,27 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class driveTrain extends SubsystemBase{
+    CANSparkMax frontRightMotor = new CANSparkMax(Constants.FRONT_RIGHT_MOTOR_DEVICE_ID, MotorType.kBrushless);
+    CANSparkMax frontLeftMotor = new CANSparkMax(Constants.FRONT_LEFT_MOTOR_DEVICE_ID, MotorType.kBrushless);
+    CANSparkMax backRightMotor = new CANSparkMax(Constants.BACK_RIGHT_MOTOR_DEVICE_ID, MotorType.kBrushless);
+    CANSparkMax backLeftMotor = new CANSparkMax(Constants.BACK_LEFT_MOTOR_DEVICE_ID, MotorType.kBrushless);
+    RelativeEncoder frontRightEncoder = frontRightMotor.getEncoder();
+    RelativeEncoder frontLeftEncoder = frontLeftMotor.getEncoder();
+    RelativeEncoder backRightEncoder = backRightMotor.getEncoder();
+    RelativeEncoder backLeftEncoder = backLeftMotor.getEncoder();
+
+    MotorControllerGroup rightDrive = new MotorControllerGroup(frontRightMotor, backRightMotor);
+    MotorControllerGroup leftDrive = new MotorControllerGroup(frontLeftMotor, backLeftMotor);
+    MotorControllerGroup allDrive = new MotorControllerGroup(rightDrive, leftDrive);
+    public void drive(){
+        
+    }
+}

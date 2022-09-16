c08a85f6cf0564b8ebc3c25e052a79c835a3f998
EmilyKram <90582499+EmilyKram@users.noreply.github.com>
2022-09-16 17:22:38 -0500

    Update Limelight.java

--- New Robot Timed/src/main/java/frc/robot/subsystems/Limelight.java
+++ New Robot Timed/src/main/java/frc/robot/subsystems/Limelight.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Limelight extends SubsystemBase {
@@ -44,6 +45,7 @@ public class Limelight extends SubsystemBase {
         pipeline.setDouble(limelightMode.getModeValue());
     }
     public boolean hasTarget(){
+        SmartDashboard.putBoolean("Has Target", tv.getBoolean(false));
         return tv.getBoolean(false);
         }
 

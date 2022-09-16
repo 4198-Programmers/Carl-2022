1f6e0e3e32a20f3e32ad79b6c1afa825d852c30b
EmilyKram <90582499+EmilyKram@users.noreply.github.com>
2022-09-16 18:56:32 -0500

    Create Intake.java

---
+++ New Robot Timed/src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Intake extends SubsystemBase{
+    CANSparkMax intakeMotor = new CANSparkMax(Constants.INTAKE_MOTOR_DEVICE_ID, MotorType.kBrushless);
+    public void setintakespeed(double speed){
+        intakeMotor.set(speed);
+    }
+}

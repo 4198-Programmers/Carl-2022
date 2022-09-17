39630e95e4ba1af0a585f93e75b3f82cfcdc9a73
EmilyKram <90582499+EmilyKram@users.noreply.github.com>
2022-09-16 19:08:54 -0500

    Create AngledHook.java

---
+++ New Robot Timed/src/main/java/frc/robot/simpleCommands/HookCommands/AngledHook.java
@@ -0,0 +1,26 @@
+package frc.robot.simpleCommands.HookCommands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.AngledHooks;
+
+public class AngledHook extends CommandBase{
+    AngledHooks angledHooks;
+    public interface hookSpeedRetriever{
+        double getangledspeed();
+    }
+    hookSpeedRetriever angledhookSpeedRetriever;
+    public AngledHook(AngledHooks angledHooks, hookSpeedRetriever angledhookSpeedRetriever){
+        this.angledHooks = angledHooks;
+        this.angledhookSpeedRetriever = angledhookSpeedRetriever;
+        addRequirements(angledHooks);
+    }
+    @Override
+    public void execute() {
+        angledHooks.setAngledHookSpeed(angledhookSpeedRetriever.getangledspeed());
+    }
+    @Override
+    public void end(boolean interrupted) {
+        angledHooks.setAngledHookSpeed(Constants.FREEZE);
+    }
+}

763903e98e5bd6968c69a61183d5627c9b0d9751
EmilyKram <90582499+EmilyKram@users.noreply.github.com>
2022-11-19 09:08:55 -0600

    Update Drivetrain.java

--- New Robot Timed/src/main/java/frc/robot/Drivetrain.java
+++ New Robot Timed/src/main/java/frc/robot/Drivetrain.java
@@ -1,8 +1,10 @@
 package frc.robot;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.Constants;
@@ -16,6 +18,21 @@ public class Drivetrain extends SubsystemBase {
     MotorControllerGroup leftMotors = new MotorControllerGroup(frontLMotor, backLMotor);
     MotorControllerGroup rightMotors = new MotorControllerGroup(frontRMotor, backRMotor);
 
+    /**Front Left Motor Encoder */
+    RelativeEncoder frontLMEnc = frontLMotor.getEncoder();
+    RelativeEncoder frontRMEnc = frontRMotor.getEncoder();
+    RelativeEncoder backRMEnc = backLMotor.getEncoder();
+    RelativeEncoder backLMEnc = backLMotor.getEncoder();
     
-    
+    DifferentialDrive drive = new DifferentialDrive(leftMotors, rightMotors);
+    public void driveBoy(double zRotation, double xSpeed){
+
+        drive.arcadeDrive(zRotation, xSpeed);
+
+    }
+
+    public double findPosition(){
+
+        
+    }
 }

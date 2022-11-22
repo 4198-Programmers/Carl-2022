976d3b6231ecd5502712fdcec7e6bc5c8182c38b
EmilyKram <90582499+EmilyKram@users.noreply.github.com>
2022-11-22 12:35:25 -0600

    Update RobotContainer.java
    I have just written down a few notes

--- New Robot Timed/src/main/java/frc/robot/RobotContainer.java
+++ New Robot Timed/src/main/java/frc/robot/RobotContainer.java
@@ -1,5 +1,16 @@
 package frc.robot;
+/* 
+This is just a message for everyone.
+Robot and Main do not get edited at all when programming.  Never touch the two.  If something in one of the two
+is deleted, the program will never work.
+Another note:
+  Always use names that make sense.  If you are talking about the front left motor, maybe name it "FrontLMotor",
+  If you are unsure if someone would understand, comment on it.  
+  Make a comment(like this one) and explain what it means.
 
+If you have any questions, do not be afraid to ask me.  I will help you figure it out and if I don't know, I will
+figure it out with you.
+*/
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 
@@ -8,29 +19,35 @@ public class RobotContainer {
 
 
   // subsystems
+    //This is just where you would pass your subsystems to robot container.
 
-  
+
   // ungrouped commands
+    //This is the commands that you make in other files.
   
 
 
   // command groups
-//RunCommand drivesticks = new RunCommand(()-> drivetrain.driveBoy(middleJoystick.getRawAxis(0), leftJoystick.getRawAxis(1)), drivetrain);
+    //These are commands that you make here in RobotContainer.  I will explain that uses for these later.
+
   //Buttons
+    //This is where you make your buttons.  You do not assign what the buttons do here, but you create them.
   
   
   // others
   private final SendableChooser<Command> m_chooser = new SendableChooser<>();
-
+/**This is Robot Container Initialize. This initializes once when the robot starts up.*/
   public void initialize() {
     configureButtonBindings();
     begin();
   }
-
+/**This is where you assign your buttons to commands.  You make the buttons earlier, but this is where you make the
+ * buttons do something.
+ */
   private void configureButtonBindings() {
 
 }
-
+/**Nothing goes in here. This just tells the robot to "begin". */
   private void begin() {
   }
 

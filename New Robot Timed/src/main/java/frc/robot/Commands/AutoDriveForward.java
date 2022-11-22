d0d5fc470a365c37338a2a6c097841502c7cd382
EmilyKram <90582499+EmilyKram@users.noreply.github.com>
2022-11-22 12:17:00 -0600

    Create AutoDriveForward.java

---
+++ New Robot Timed/src/main/java/frc/robot/Commands/AutoDriveForward.java
@@ -0,0 +1,7 @@
+package frc.robot.Commands;
+/**This is a challenge to figure out how to make the robot move forward for a certain amount of time.
+ * Look back at your AutoDrive command to help you do this.
+ */
+public class AutoDriveForward {
+    
+}

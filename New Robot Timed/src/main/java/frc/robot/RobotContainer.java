e8dc375cbc9888bf1d59f27c8eb3225e189b5346
LeoThayer <92405073+LeoThayer@users.noreply.github.com>
2023-01-20 16:31:11 -0600

    Fixed Port Values On Joysticks.

--- New Robot Timed/src/main/java/frc/robot/RobotContainer.java
+++ New Robot Timed/src/main/java/frc/robot/RobotContainer.java
@@ -7,13 +7,12 @@ import frc.robot.commands.SecondaryMotorTesting;
 import frc.robot.subsystems.PrimaryMotor;
 import frc.robot.subsystems.SecondaryMotor;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class RobotContainer {
 
   // Joysticks
-  Joystick primaryTestJoystick = new Joystick(Constants.LEFT_SHAFT_PORT);
-  Joystick secondaryTestJoystick = new Joystick(Constants.MIDDLE_SHAFT_PORT);
+  Joystick primaryTestJoystick = new Joystick(Constants.PRIMARY_MOTOR_TEST_JOYSTICK_PORT);
+  Joystick secondaryTestJoystick = new Joystick(Constants.SECONDARY_MOTOR_TEST_JOYSTICK_PORT);
 
   // subsystems
   PrimaryMotor primaryMotor = new PrimaryMotor();

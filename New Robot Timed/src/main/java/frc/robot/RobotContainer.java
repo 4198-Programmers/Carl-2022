f92655aebc875d7efa3066d0b9bafda92b799bee
LeoThayer <92405073+LeoThayer@users.noreply.github.com>
2023-01-23 12:16:41 -0600

    Adding more Joystick Buttons to the code.

--- New Robot Timed/src/main/java/frc/robot/RobotContainer.java
+++ New Robot Timed/src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,7 @@ import frc.robot.subsystems.TurretRotationMotor;
 import frc.robot.subsystems.PincherMotor;
 import frc.robot.subsystems.TurretArmMotor;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class RobotContainer {
 
@@ -23,11 +24,21 @@ public class RobotContainer {
 
   // commands
 
+  // Joystick Buttons
+
+  JoystickButton pinchInButton = new JoystickButton(rightJoystick, Constants.PINCH_IN_BUTTON);
+  JoystickButton pinchOutButton = new JoystickButton(rightJoystick, Constants.PINCH_OUT_BUTTON);
+  JoystickButton turretArmUpButton = new JoystickButton(rightJoystick, Constants.TURRET_ARM_UP_BUTTON);
+  JoystickButton turretArmDownButton = new JoystickButton(rightJoystick, Constants.TURRET_ARM_DOWN_BUTTON);
+  JoystickButton turretRotationLeftButton = new JoystickButton(rightJoystick, Constants.TURRET_ROTATE_LEFT_BUTTON);
+  JoystickButton turretRotationRightButton = new JoystickButton(rightJoystick, Constants.TURRET_ROTATE_RIGHT_BUTTON);
+
   // other
 
   public RobotContainer() {
 
     configureButtonBindings();
+
   }
 
   private void configureButtonBindings() {
